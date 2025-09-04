4757d734e8c76f847b9dd5d4acc2329efd69f61c
Alal Hosen <alalhosen1992620120@gmail.com>
2025-09-04 23:50:06 +0600

    countries add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Countries from './components/Countries/Countries'
+
 
 function App() {
 
@@ -6,7 +8,7 @@ function App() {
     <>
       
       <h1>React World Tour</h1>
-      
+      <Countries></Countries>
     </>
   )
 }
