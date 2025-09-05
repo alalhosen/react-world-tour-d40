b99ede0fc36f7dd76bc43a462a2b9162cda0a79c
Alal Hosen <alalhosen1992620120@gmail.com>
2025-09-05 17:18:27 +0600

    population & language setup

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,7 +5,8 @@ const Countries = () => {
     const [countries, setCountries] = useState([]);
 
 useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags')
+    // fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags')
+    fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,subregion,currencies,tld,languages,borders')
     .then(res => res.json())
     .then(data => setCountries(data));
 }, [])
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,38 @@
-const Country = ({country}) => {
-const {name} = country
-    console.log(country)
-    return (
-        <div>
-            <h2>Name: {name?.common}</h2>
-        </div>
-    );
+import './Countey.css'
+
+
+// const Country = ({country}) => {
+// const {name, flags, population, area, language} = country
+//     return (
+//         <div className='country'>
+//             <h2>Name: {name?.common}</h2>
+//            <img src={flags.png} alt="" />
+//            <p>Population: {population}</p>
+//            <p>Area: {area}</p>
+//            <p>Language: {firstLanguage}</p>
+//         </div>
+//     );
+// };
+
+// export default Country;
+
+
+
+const Country = ({ country }) => {
+  const { name, flags, population, area, languages } = country;
+
+  // first language বের করা
+  const firstLanguage = languages ? Object.values(languages)[0] : "N/A";
+
+  return (
+    <div className="country">
+      <h2>Name: {name?.common}</h2>
+      <img src={flags.png} alt="" />
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
+      <p>Language: {firstLanguage}</p>
+    </div>
+  );
 };
 
 export default Country;
