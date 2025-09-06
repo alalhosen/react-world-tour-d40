c11d58b13c62d7dbfbbc3f07d5c7550c53b82125
Alal Hosen <alalhosen1992620120@gmail.com>
2025-09-06 13:25:23 +0600

    component & file of country

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,27 +1,29 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
-    const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
-useEffect(()=>{
+  useEffect(() => {
     // fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags')
-    fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,subregion,currencies,tld,languages,borders')
-    .then(res => res.json())
-    .then(data => setCountries(data));
-}, [])
+    fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital,subregion,currencies,tld,languages,borders"
+    )
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-
-
-    return (
-        <div>
-            <h3>Countries: {countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
-            
-        </div>
-    );
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  ); 
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,5 +1,5 @@
-import './Countey.css'
-
+import { useState } from "react";
+import "./Countey.css";
 
 // const Country = ({country}) => {
 // const {name, flags, population, area, language} = country
@@ -16,21 +16,29 @@ import './Countey.css'
 
 // export default Country;
 
-
-
 const Country = ({ country }) => {
-  const { name, flags, population, area, languages } = country;
+  const { name, flags, population, area, languages, capital, tid,} =
+    country;
 
   // first language বের করা
   const firstLanguage = languages ? Object.values(languages)[0] : "N/A";
 
+const [visited, setVisited] = useState(false);
+const handleVisited = () =>{
+  setVisited(!visited);
+}
+
   return (
     <div className="country">
-      <h2>Name: {name?.common}</h2>
+      <h2>Name : {name?.common}</h2>
+      <p>Language : {firstLanguage}</p>
+      <p>Capital : {capital}</p>
+      <p>Population : {population}</p>
+      <p>Area : {area}</p>
+      <p>Tid : {tid}</p>
       <img src={flags.png} alt="" />
-      <p>Population: {population}</p>
-      <p>Area: {area}</p>
-      <p>Language: {firstLanguage}</p>
+      <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+      {visited ?'I have visited this country':'I want to visit.'}
     </div>
   );
 };
